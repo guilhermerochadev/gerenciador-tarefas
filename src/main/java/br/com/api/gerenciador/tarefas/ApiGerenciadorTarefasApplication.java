77808e10e9b0960d2bc6d6ae89104c6531726c89
Guilherme Rocha <guilhermerochadev@gmail.com>
2024-12-10 16:13:19 -0300

    Criação do projeto inicial

---
+++ src/main/java/br/com/api/gerenciador/tarefas/ApiGerenciadorTarefasApplication.java
@@ -0,0 +1,13 @@
+package br.com.api.gerenciador.tarefas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGerenciadorTarefasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGerenciadorTarefasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/api/gerenciador/tarefas/ApiGerenciadorTarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.api.gerenciador.tarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGerenciadorTarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
