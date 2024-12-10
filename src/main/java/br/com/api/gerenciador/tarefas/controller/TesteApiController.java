164328b22f5efc227c3ea8cd0c774437c23b7655
Guilherme Rocha <guilhermerochadev@gmail.com>
2024-12-10 17:38:47 -0300

    Criação de uma controller de teste

--- src/main/java/br/com/api/gerenciador/tarefas/controller/TesteApiController.java
+++ src/main/java/br/com/api/gerenciador/tarefas/controller/TesteApiController.java
@@ -1,6 +1,7 @@
 package br.com.api.gerenciador.tarefas.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +10,9 @@ public class TesteApiController {
     @GetMapping("/teste-api")
     private String teste(){
         return "Sucesso";
+    }
+    @GetMapping("/teste-api-bem-vindo")
+    private String testeBemVindo(@RequestParam(name = "nome") String nome){
+        return "Olá " + nome + ", Seja muito bem vindo! ";
     }
 }
